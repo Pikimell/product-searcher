e11760d38ee600122a1086db5f74858943169ae3
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-02-29 18:33:49 +0200

    update

--- src/js/render.js
+++ src/js/render.js
@@ -4,20 +4,22 @@ function productTemplate(product, i) {
         <td>${i + 1}</td>
         <td>${product.product_name}</td>
         <td>${product.source}</td>
-        <td><a href="${product.source_link}" target="_blank">${
-    product.source_link
-  }</a></td>
-        <td><a href="${product.link_1}" target="_blank">${
-    product.link_1
-  }</a></td>
-        <td><a href="${product.aliexpress}" target="_blank">ALIEXPRESS</a></td>
+        <td><a href="${product.source_link}">Source Link</a></td>
+        <td>
+        <a href="${product.link_1}">Store-1</a>
+        <a href="${product.link_2}">Store-2</a>
+        <a href="${product.link_3}">Store-3</a>
+        </td>
+        <td><a href="${product.aliexpress}">ALIEXPRESS</a></td>
         <td class="table-border"></td>
         <td>${product.orders}</td>
         <td>${product.popularity}</td>
         <td>${product.reviews}</td>
-        <td>${product.youtube_media ? 'Yes' : 'No'}</td>
+        <td><input type="checkbox" ${
+          product.youtube_media ? 'checked' : ''
+        }></td>
         <td class="table-border"></td>
-        <td>${product.supplier_link}</td>
+        <td><a href="${product.supplier_link}">Supplier Link</a></td>
         <td>${product.supplier_price}</td>
         <td>${product.sale_price}</td>
         <td>${product.competitor_price}</td>

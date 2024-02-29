29fde9b53db3d37f5be5dab8c1bdbf5abcf47ff0
GoITteacher <50087029+GoITteacher@users.noreply.github.com>
2024-02-29 15:51:14 +0000

    Deploying to gh-pages from @ Pikimell/product-searcher@7a6d72e5f1da4d47da9d12ab91d6a95f7c0d6738 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,25 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{l as a}from"./assets/helpers-19b93edd.js";function d(t,e){return`
+      <tr>
+        <td>${e+1}</td>
+        <td>${t.product_name}</td>
+        <td>${t.source}</td>
+        <td><a href="${t.source_link}" target="_blank">${t.source_link}</a></td>
+        <td><a href="${t.link_1}" target="_blank">${t.link_1}</a></td>
+        <td><a href="${t.aliexpress}" target="_blank">ALIEXPRESS</a></td>
+        <td class="table-border"></td>
+        <td>${t.orders}</td>
+        <td>${t.popularity}</td>
+        <td>${t.reviews}</td>
+        <td>${t.youtube_media?"Yes":"No"}</td>
+        <td class="table-border"></td>
+        <td>${t.supplier_link}</td>
+        <td>${t.supplier_price}</td>
+        <td>${t.sale_price}</td>
+        <td>${t.competitor_price}</td>
+        <td>${t.comments}</td>
+        <td>${t.margin}</td>
+        <td>${t.niche}</td>
+        <td>${t.points}</td>
+      </tr>
+    `}function n(t){return t.map(d).join("")}const i={tableBody:document.querySelector("tbody")},l=a("user-data")||[];function r(){const t=n(l);i.tableBody.innerHTML=t}r();
 //# sourceMappingURL=commonHelpers.js.map

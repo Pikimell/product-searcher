1a8fb74d5cf604b18d92d522519d6d05ec494499
GO IT Teacher <50087029+GoITteacher@users.noreply.github.com>
2024-02-29 22:40:54 +0200

    update

--- src/js/render.js
+++ src/js/render.js
@@ -1,23 +1,29 @@
 function productTemplate(product, i) {
   const validOrders = parseInt(product.orders) > 500;
-  const validPopularity = parseInt(product.orders) > 500;
-  const validMedia = parseInt(product.orders) > 500;
-  // const isValid = ;
+  const validPopularity = parseInt(product.popularity) > 50;
+  const validMedia = product.youtube_media;
+  const isValid = validOrders && validPopularity && validMedia;
 
-  // const myClass = isValid ? 'valid' : 'invalid';
+  const myClass = isValid ? 'valid' : 'invalid';
 
   return `
-      <tr>
+      <tr class=${myClass}>
         <td>${i + 1 || ''}</td>
         <td>${product.product_name || ''}</td>
         <td>${product.source || ''}</td>
-        <td><a href="${product.source_link || ''}">Source Link</a></td>
+
         <td>
-        <a href="${product.link_1 || ''}">Store-1</a>
-        <a href="${product.link_2 || ''}">Store-2</a>
-        <a href="${product.link_3 || ''}">Store-3</a>
+        ${product.link_1 ? `<a href="${product.link_1}">Store-1</a>` : ''}
+        ${product.link_2 ? `<a href="${product.link_2}">Store-2</a>` : ''}
+        ${product.link_3 ? `<a href="${product.link_3}">Store-3</a>` : ''}
         </td>
-        <td><a href="${product.aliexpress || ''}">ALIEXPRESS</a></td>
+        
+        <td>${
+          product.aliexpress
+            ? `<a href="${product.aliexpress}">ALIEXPRESS</a>`
+            : ''
+        }</td>
+        
         <td class="table-border"></td>
         <td>${product.orders || ''}</td>
         <td>${product.popularity || ''}</td>
@@ -26,7 +32,12 @@ function productTemplate(product, i) {
           product.youtube_media ? 'checked' : ''
         }></td>
         <td class="table-border"></td>
-        <td><a href="${product.supplier_link || ''}">Supplier Link</a></td>
+
+        <td>${
+          product.supplier_link
+            ? `<a href="${product.supplier_link}">Supplier Link</a>`
+            : ''
+        }</td>
         <td>${product.supplier_price || ''}</td>
         <td>${product.sale_price || ''}</td>
         <td>${product.competitor_price || ''}</td>
